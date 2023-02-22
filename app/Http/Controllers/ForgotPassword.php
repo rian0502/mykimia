4e512726da0b6abf2a8c60b811aaa8dd69f61ec7
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2023-02-22 19:31:38 +0700

    first commit

---
+++ app/Http/Controllers/ForgotPassword.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+
+class ForgotPassword extends Controller
+{
+    //
+
+    public function index(){
+        return view('formemail');
+    }
+    public function sendResetLinkEmail(Request $request){
+        $response = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+            ? back()->with(['status' => __($response)])
+            : back()->with(['email' => __($response)]);
+    }
+    public function showResetPasswordForm($token){
+        return dd($token);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ForgotPassword;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/forgot-password', [ForgotPassword::class, 'index'])
+    ->name('password.request');
+Route::post('/forgot-password', [ForgotPassword::class, 'sendResetLinkEmail'])
+    ->name('password.email');
+Route::get('reset-password/{token}', [ForgotPassword::class, 'showResetPasswordForm'])
+    ->name('password.reset');
