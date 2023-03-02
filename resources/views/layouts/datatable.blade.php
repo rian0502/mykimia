e18b821ca9d4ef241672f27a621c6d6ed69b0e15
PutuPersada <putupersada@gmail.com>
2023-03-02 18:39:34 +0700

    update

--- resources/views/layouts/datatable.blade.php
+++ resources/views/layouts/datatable.blade.php
@@ -522,7 +522,7 @@
 
 
     </div>
-    @yield('admin')
+    @yield('datatable')
 
 
 
@@ -542,9 +542,16 @@
             style="display: none; visibility: hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
-
-
-
+    <!-- buttons for Export datatable -->
+    <script src="/Assets/admin/src/plugins/datatables/js/dataTables.buttons.min.js"></script>
+    <script src="/Assets/admin/src/plugins/datatables/js/buttons.bootstrap4.min.js"></script>
+    <script src="/Assets/admin/src/plugins/datatables/js/buttons.print.min.js"></script>
+    <script src="/Assets/admin/src/plugins/datatables/js/buttons.html5.min.js"></script>
+    <script src="/Assets/admin/src/plugins/datatables/js/buttons.flash.min.js"></script>
+    <script src="/Assets/admin/src/plugins/datatables/js/pdfmake.min.js"></script>
+    <script src="/Assets/admin/src/plugins/datatables/js/vfs_fonts.js"></script>
+    <!-- Datatable Setting js -->
+    <script src="/Assets/admin/vendors/scripts/datatable-setting.js"></script>
 </body>
 
 </html>
