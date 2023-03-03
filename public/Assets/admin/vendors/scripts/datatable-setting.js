c327654552e563b9170a27a7c101e59c7416ab6d
PutuPersada <putupersada@gmail.com>
2023-03-03 09:17:12 +0700

    new

--- public/Assets/admin/vendors/scripts/datatable-setting.js
+++ public/Assets/admin/vendors/scripts/datatable-setting.js
@@ -35,7 +35,7 @@ $("document").ready(function () {
         ],
         lengthMenu: [
             [10, 25, 50, -1],
-            [10, 25, 50, "All"],
+            [10, 25, 50, "Semua"],
         ],
         language: {
             info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ data",
